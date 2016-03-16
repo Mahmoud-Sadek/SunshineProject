315d05b927dab7dfe7f52b9936b0016e7100b121
Mahmoud <mahmoud.sadek.96@gmail.com>
2016-03-16 23:53:53 +0200

    second Lesson

--- app/src/main/java/com/example/mahmoud/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/mahmoud/sunshine/ForecastFragment.java
@@ -48,7 +48,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add this line in order for this fragment to handle menu events.
+
         setHasOptionsMenu(true);
     }
 
